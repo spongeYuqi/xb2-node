9c173ccf47348580ff201c0d39c779a97be2b793
zhouyuqi <2280356057@qq.com>
2024-09-16 10:52:57 +0800

    '创建web服务器'

--- src/main.js
+++ src/main.js
@@ -1,2 +1,14 @@
-const greeting = '你好';
-console.log(greeting);
+// const greeting = '你好';
+// console.log(greeting);
+
+//导入模块，使用node.js的http功能
+const http = require('http');
+//创建服务器
+const server = http.createServer((request,Response) => {
+    Response.write('hello~');
+    Response.end();//结束响应
+});
+//监听  3000为端口号
+server.listen(3000,() => {
+    console.log('🐱‍🚀服务已启动');
+})
